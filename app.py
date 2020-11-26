4df07cde46193dfbfe8f37a46372a0f8a4fed2bd
sammysimmons <sammysimmons274@gmail.com>
2020-11-26 15:09:17 +0000

    redirect login to the dashboard

--- app.py
+++ app.py
@@ -61,8 +61,7 @@ def login():
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
-                        flash("Welcome, {}".format(request.form.get("username")))
-                        return redirect(url_for("profile", username=session["user"]))
+                        return redirect(url_for("dashboard", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
