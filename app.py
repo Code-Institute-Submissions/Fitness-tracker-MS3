2beb18020bebc62c5073e6d8f7061160d4563b88
sammysimmons <sammysimmons274@gmail.com>
2020-12-07 19:43:54 +0000

    challenge friend templating

--- app.py
+++ app.py
@@ -102,13 +102,14 @@ def login():
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
-    users = mongo.db.users.find_one(
+    username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-   
+
     if session["user"]:
         return render_template("profile.html", username=username)
 
     return redirect(url_for("login"))
+
 
 @app.route("/logout")
 def logout(): 
