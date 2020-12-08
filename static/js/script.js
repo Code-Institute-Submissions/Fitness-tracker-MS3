aa8c664e57d9e3b15015778c32c957ea790e8966
sammysimmons <sammysimmons274@gmail.com>
2020-12-08 22:04:06 +0000

    git commit

--- static/js/script.js
+++ static/js/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
             done: "Select"
         }
     });
+
     $('select').formSelect();
     $('#textarea1').val('New Text');
     M.textareaAutoResize($('#textarea1'));
