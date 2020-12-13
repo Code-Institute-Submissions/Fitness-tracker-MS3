2c5e4ce3403c395aabe467e76c1286162d65dc08
sammysimmons <sammysimmons274@gmail.com>
2020-12-13 18:24:51 +0000

    testing random imagery load on adding workout

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,14 @@ $(document).ready(function() {
     $('#textarea1').val('New Text');
     M.textareaAutoResize($('#textarea1'));
 });
+
+
+/*
+    image random generator
+*/
+var images = ["workout1.jpg", "workout2.jpg", "workout3.jpg", "workout4.jpg", "workout5.jpg", "workout6.jpg"]
+
+function chose() {
+    var rand = Math.floor(Math.random() * images.length);
+    document.getElementById("workoutImage").src = images[rand];
+}
